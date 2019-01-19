2fc1a4bf3e1dfd06039b7ff2210688a40aafb05a
huangjiacheng <1422878799@qq.com>
2019-01-19 15:44:31 +0800

    黄家诚
    首次上传

---
+++ registered.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        print '欢迎$_post['username']';
+        print '<br>';
+        print '您的账号为$_post['Account']';
+    ?>
+</body>
+</html>
