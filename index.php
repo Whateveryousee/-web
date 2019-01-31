3cf30633ab3821c19f10dc36b78696e8cc6c7bbe
huangjiacheng <1422878799@qq.com>
2019-01-31 09:17:04 +0800

    家诚
    0.0.4

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    欢迎!<br>
+    你的账号：<?php echo $_POST["useraccount"]; ?><br>
+    你的密码：<?php echo $_POST["userpassword"]; ?><br>
+    3秒后跳转至登录界面<br>
+    或者你可以直接<a href="login.html">点击</a>跳转
+    <script>
+        setInterval(function(){
+            window.location.href="index.html";
+        }, 3000);
+    </script>
+</body>
+</html>
--- registered.php
+++ registered.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,10 +7,15 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        print '欢迎$_post['username']';
-        print '<br>';
-        print '您的账号为$_post['Account']';
-    ?>
+    欢迎<?php echo $_POST["username"]; ?>!<br>
+    你的账号：<?php echo $_POST["useraccount"]; ?><br>
+    你的密码：<?php echo $_POST["userpassword"]; ?><br>
+    3秒后跳转至登录界面<br>
+    或者你可以直接<a href="login.html">点击</a>跳转
+    <script>
+        setInterval(function(){
+            window.location.href="login.html";
+        }, 3000);
+    </script>
 </body>
 </html>
